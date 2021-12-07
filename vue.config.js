5ce7dd474f0661174891f85b14947124a47f41a0
Luke.Deng <zx20170401@126.com>
2021-12-07 18:41:07 +0800

    组织机构页面完成

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,7 @@ const GitRevisionPlugin = require('git-revision-webpack-plugin');
 const GitRevision = new GitRevisionPlugin();
 const buildDate = JSON.stringify(new Date().toLocaleString());
 const createThemeColorReplacerPlugin = require('./config/plugin.config');
-// const url = 'http://localhost:9999';
-const url = 'http://10.10.10.118:9999/';
+const url = 'http://localhost:9999';
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
